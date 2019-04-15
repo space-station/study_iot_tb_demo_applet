5718b728c11aab7849bcf08e30844a695a0dbc72
ZhuXueli <xueli.zhu@borqs.com>
2019-04-15 10:37:13 +0800

    Adding asset and customer interface in thingsboard lib

--- wx/thingsboard_test/pages/index/things_board_util.js
+++ wx/thingsboard_test/pages/index/things_board_util.js
@@ -48,6 +48,7 @@ function findDevice(deviceName) {
 
 /**
  * create device
+ * TODO need more device information in parameter
  * @param {string} token: login token
  * @param {string} deviceName: created device name
  * @param {function} callback: callback for success or fail. param contain (boolean isSuccess, Object resData)
@@ -166,22 +167,277 @@ function deleteDeviceById(token, id, callback) {
     },
     fail(res) {
       if (debug) console.log('deleteDeviceById fail')
+      callback(false)
+    }
+  })
+}
+
+/**
+ * create asset
+ * TODO need more asset information in parameter
+ * @param {string} token: login token
+ * @param {string} assetName: created asset name
+ * @param {function} callback: callback for success or fail. param contain (boolean isSuccess, Object resData)
+ */
+function createAsset(token, assetName, callback) {
+  wx.request({
+    url: mServerAddress + '/api/asset',
+    data: {
+      name: assetName,
+      type: 'default',
+    },
+    header: {
+      'X-Authorization': 'Bearer ' + token,
+    },
+    method: 'POST',
+    success(res) {
+      if (debug) console.log('createAsset success status code ' + res.statusCode)
+      if (res.statusCode == HTTP_UNAUTHORIZED) {
+        callback(false, res.data)
+      } else {
+        callback(true, res.data)
+      }
+    },
+    fail(res) {
+      if (debug) console.log('createAsset fail')
+      callback(false, null)
+    },
+  })
+}
+
+/**
+ * get asset info by id
+ * @param {string} token: login token
+ * @param {string} id: asset id
+ * @param {function} callback: callback for success or fail. param contain (boolean isSuccess, Object assetData)
+ */
+function getAssetById(token, id, callback) {
+  if (debug) console.log('getAssetById id ' + id)
+  wx.request({
+    url: mServerAddress + '/api/asset/' + id,
+    header: {
+      'X-Authorization': 'Bearer ' + token,
+    },
+    method: 'GET',
+    success(res) {
+      if (debug) console.log('getAssetById success status code ' + res.statusCode)
+      if (res.statusCode == HTTP_UNAUTHORIZED) {
+        callback(false, res.data)
+      } else if (res.statusCode == HTTP_NOT_FOUND) {
+        callback(false, res.data)
+      } else {
+        callback(true, res.data)
+      }
+    },
+    fail(res) {
+      if (debug) console.log('getAssetById fail')
       callback(false, null)
     }
   })
 }
 
 /**
+ * get asset info by name
+ * @param {string} token: login token
+ * @param {string} name: asset name
+ * @param {function} callback: callback for success or fail. param contain (boolean isSuccess, Object assetData)
+ */
+function getAssetByName(token, name, callback) {
+  if (debug) console.log('getAssetByName name ' + name)
+  wx.request({
+    url: mServerAddress + '/api/tenant/assets?assetName=' + name,
+    header: {
+      'X-Authorization': 'Bearer ' + token,
+    },
+    method: 'GET',
+    success(res) {
+      if (debug) console.log('getAssetByName success status code ' + res.statusCode)
+      if (res.statusCode == HTTP_UNAUTHORIZED) {
+        callback(false, res.data)
+      } else if (res.statusCode == HTTP_NOT_FOUND) {
+        callback(false, res.data)
+      } else {
+        callback(true, res.data)
+      }
+    },
+    fail(res) {
+      if (debug) console.log('getAssetByName fail')
+      callback(false, null)
+    }
+  })
+}
+
+/**
+ * delete asset by id
+ * @param {string} token: login token
+ * @param {string} id: asset id
+ * @param {function} callback: callback for success or fail. param contain (boolean isSuccess)
+ */
+function deleteAssetById(token, id, callback) {
+  if (debug) console.log('deleteAssetById id ' + id)
+  wx.request({
+    url: mServerAddress + '/api/asset/' + id,
+    header: {
+      'X-Authorization': 'Bearer ' + token,
+    },
+    method: 'DELETE',
+    success(res) {
+      if (debug) console.log('deleteAssetById success status code ' + res.statusCode)
+      if (res.statusCode == HTTP_UNAUTHORIZED) {
+        callback(false)
+      } else if (res.statusCode == HTTP_NOT_FOUND) {
+        callback(false)
+      } else {
+        callback(true)
+      }
+    },
+    fail(res) {
+      if (debug) console.log('deleteAssetById fail')
+      callback(false)
+    }
+  })
+}
+
+/**
+ * create customer
+ * TODO need more customer information in parameter
+ * @param {string} token: login token
+ * @param {string} title: created customer title
+ * @param {function} callback: callback for success or fail. param contain (boolean isSuccess, Object resData)
+ */
+function createCustomer(token, title, callback) {
+  wx.request({
+    url: mServerAddress + '/api/customer',
+    data: {
+      title: title,
+    },
+    header: {
+      'X-Authorization': 'Bearer ' + token,
+    },
+    method: 'POST',
+    success(res) {
+      if (debug) console.log('createCustomer success status code ' + res.statusCode)
+      if (res.statusCode == HTTP_UNAUTHORIZED) {
+        callback(false, res.data)
+      } else {
+        callback(true, res.data)
+      }
+    },
+    fail(res) {
+      if (debug) console.log('createCustomer fail')
+      callback(false, null)
+    },
+  })
+}
+
+/**
+ * get customer info by id
+ * @param {string} token: login token
+ * @param {string} id: customer id
+ * @param {function} callback: callback for success or fail. param contain (boolean isSuccess, Object customerData)
+ */
+function getCustomerById(token, id, callback) {
+  if (debug) console.log('getCustomerById id ' + id)
+  wx.request({
+    url: mServerAddress + '/api/customer/' + id,
+    header: {
+      'X-Authorization': 'Bearer ' + token,
+    },
+    method: 'GET',
+    success(res) {
+      if (debug) console.log('getCustomerById success status code ' + res.statusCode)
+      if (res.statusCode == HTTP_UNAUTHORIZED) {
+        callback(false, res.data)
+      } else if (res.statusCode == HTTP_NOT_FOUND) {
+        callback(false, res.data)
+      } else {
+        callback(true, res.data)
+      }
+    },
+    fail(res) {
+      if (debug) console.log('getCustomerById fail')
+      callback(false, null)
+    }
+  })
+}
+
+/**
+ * get customer info by title
+ * @param {string} token: login token
+ * @param {string} title: customer title
+ * @param {function} callback: callback for success or fail. param contain (boolean isSuccess, Object customerData)
+ */
+function getCustomerByTitle(token, title, callback) {
+  if (debug) console.log('getCustomerByTitle title ' + title)
+  wx.request({
+    url: mServerAddress + '/api/tenant/customers?customerTitle=' + title,
+    header: {
+      'X-Authorization': 'Bearer ' + token,
+    },
+    method: 'GET',
+    success(res) {
+      if (debug) console.log('getCustomerByTitle success status code ' + res.statusCode)
+      if (res.statusCode == HTTP_UNAUTHORIZED) {
+        callback(false, res.data)
+      } else if (res.statusCode == HTTP_NOT_FOUND) {
+        callback(false, res.data)
+      } else {
+        callback(true, res.data)
+      }
+    },
+    fail(res) {
+      if (debug) console.log('getCustomerByTitle fail')
+      callback(false, null)
+    }
+  })
+}
+
+/**
+ * delete customer by id
+ * @param {string} token: login token
+ * @param {string} id: customer id
+ * @param {function} callback: callback for success or fail. param contain (boolean isSuccess)
+ */
+function deleteCustomerById(token, id, callback) {
+  if (debug) console.log('deleteCustomerById id ' + id)
+  wx.request({
+    url: mServerAddress + '/api/customer/' + id,
+    header: {
+      'X-Authorization': 'Bearer ' + token,
+    },
+    method: 'DELETE',
+    success(res) {
+      if (debug) console.log('deleteCustomerById success status code ' + res.statusCode)
+      if (res.statusCode == HTTP_UNAUTHORIZED) {
+        callback(false)
+      } else if (res.statusCode == HTTP_NOT_FOUND) {
+        callback(false)
+      } else {
+        callback(true)
+      }
+    },
+    fail(res) {
+      if (debug) console.log('deleteCustomerById fail')
+      callback(false)
+    }
+  })
+}
+
+
+/**
  * test all function
  */
 function utilTest() {
   init('http://192.168.4.119:8080')
+  //init('https://lianluotu.cn')
   var mToken = ''
   login('tenant@thingsboard.org', 'tenant',
     function(isSuccess, token) {
       console.log('login test isSuccess ' + isSuccess + ' token ' + token)
       mToken = token
       testDevice(mToken)
+      testAsset(mToken)
+      testCusomer(mToken)
     })
 }
 
@@ -215,6 +471,64 @@ function testDevice(token) {
 }
 
 /**
+ * test case about asset
+ */
+function testAsset(token) {
+  createAsset(token, "test", function(isSuccess, resData) {
+    printMapObject('testAsset createAsset', resData)
+
+    getAssetById(token, resData['id']['id'], function(isSuccess, resData) {
+      if (isSuccess) {
+        printMapObject('testAsset getAssetById', resData)
+      } else {
+        console.log('testAsset getAssetById fail')
+      }
+    })
+
+    getAssetByName(token, resData['name'], function(isSuccess, resData) {
+      if (isSuccess) {
+        printMapObject('testAsset getAssetByName', resData)
+      } else {
+        console.log('testAsset getAssetByName fail')
+      }
+    })
+
+    deleteAssetById(token, resData['id']['id'], function(isSuccess) {
+      console.log('testAsset deleteAssetById isSuccess ' + isSuccess)
+    })
+  })
+}
+
+/**
+ * test case about customer
+ */
+function testCusomer(token) {
+  createCustomer(token, "test", function (isSuccess, resData) {
+    printMapObject('testCusomer createCustomer', resData)
+
+    getCustomerById(token, resData['id']['id'], function (isSuccess, resData) {
+      if (isSuccess) {
+        printMapObject('testCusomer getCustomerById', resData)
+      } else {
+        console.log('testCusomer getCustomerById fail')
+      }
+    })
+
+    getCustomerByTitle(token, resData['title'], function (isSuccess, resData) {
+      if (isSuccess) {
+        printMapObject('testCusomer getCustomerByTitle', resData)
+      } else {
+        console.log('testCusomer getCustomerByTitle fail')
+      }
+    })
+
+    deleteCustomerById(token, resData['id']['id'], function (isSuccess) {
+      console.log('testCusomer deleteCustomerById isSuccess ' + isSuccess)
+    })
+  })
+}
+
+/**
  * print map object for json data
  * @param {string} tag: log tag
  * @param {Object} map: type data
@@ -232,5 +546,13 @@ module.exports = {
   getDeviceById: getDeviceById,
   getDeviceByName: getDeviceByName,
   deleteDeviceById: deleteDeviceById,
+  createAsset: createAsset,
+  getAssetById: getAssetById,
+  getAssetByName: getAssetByName,
+  deleteAssetById: deleteAssetById,
+  createCustomer: createCustomer,
+  getCustomerById: getCustomerById,
+  getCustomerByTitle: getCustomerByTitle,
+  deleteCustomerById: deleteCustomerById,
   utilTest: utilTest,
 }
